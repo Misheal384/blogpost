e7bfd769e43f9a03cfe24f5b914199be0a86701d
Misheal384 <eversonmisheal2@gmail.com>
2024-12-31 11:11:16 +0000

    updated vercel.json and backend configurations for deployment

--- blogpost-crud/index.js
+++ blogpost-crud/index.js
@@ -4,7 +4,10 @@ const blogRoutes = require('./routes/blogRoutes');
 const dotenv = require('dotenv');
 const authRoutes = require("./routes/authRoutes");
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,11 +15,22 @@ app.use(bodyParser.json());
 
 const cors = require('cors');
 app.use(cors({
-    origin: 'http://localhost:3000' 
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000' 
 }));
+
 
 app.use("/auth", authRoutes);
 app.use('/api', blogRoutes);
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'Server is healthy!' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
